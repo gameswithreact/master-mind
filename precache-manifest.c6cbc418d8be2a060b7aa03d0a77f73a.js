8522d76e9e417de93f5d90e7b355e628dd7695ec
Michael Kosak <gameswithreact@rambler.ru>
2019-01-09 22:47:32 +0100

    Updates

---
+++ precache-manifest.c6cbc418d8be2a060b7aa03d0a77f73a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8d2da52a75131bcb9186f31d55da247",
+    "url": "/master-mind/static/media/sideBoardOpacity.a8d2da52.png"
+  },
+  {
+    "revision": "94df11a06f388e0072f0",
+    "url": "/master-mind/static/js/runtime~main.94df11a0.js"
+  },
+  {
+    "revision": "21253b837694febc77ab",
+    "url": "/master-mind/static/js/main.21253b83.chunk.js"
+  },
+  {
+    "revision": "a287b82f5b26599b3926",
+    "url": "/master-mind/static/js/1.a287b82f.chunk.js"
+  },
+  {
+    "revision": "21253b837694febc77ab",
+    "url": "/master-mind/static/css/main.25bc4a90.chunk.css"
+  },
+  {
+    "revision": "3650a2bcbccc9f8cb1d4eb337ea500c2",
+    "url": "/master-mind/index.html"
+  }
+];
